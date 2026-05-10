03fa81ab4e9a1ebab61efaaa8336ef1e229406ee
ajitesh2405 <ajitesh@priyaqubit.onmicrosoft.com>
2026-05-10 17:34:42 +0530

    second commit

--- src/app.js
+++ src/app.js
@@ -11,6 +11,15 @@ app.get("/user", (req, res) => {
     res.send({firstName: "Ajitesh", lastName: "Sharma"});
 });
 
+// app.get("/user/:userId/:userName", (req, res) => {
+        // : means that userId and userName are route parameters that can be accessed using req.params,
+        //  it is a dynamic route that can handle requests with different userId and userName values
+//     console.log(req.query); //Accessing query parameters from the request
+//     console.log(req.params); //Accessing route parameters from the request
+//     res.send({firstName: "Ajitesh", lastName: "Sharma"});
+
+// });
+
 app.post("/user", (req, res) => {
     //Saving data to database
     res.send("Data has been saved to the database");
@@ -19,7 +28,31 @@ app.post("/user", (req, res) => {
 app.delete("/user", (req, res) => {
     //Deleting data from database
     res.send("Data has been deleted from the database");
-})
+});
+
+// app.get("ab ? c", (req, res) => {
+//     res.send("ab?c");
+// });
+
+// app.get("ab+c", (req, res) => {
+//     res.send("ab+c");
+// });
+
+// app.get("ab*c", (req, res) => {
+//     res.send("ab*c");
+// });
+
+// app.get("ab(cd)?e", (req, res) => {
+//     res.send("ab(cd)?e");
+// });
+
+// app.get(/a/, (req, res) => {
+//     res.send("/a/");
+// });
+
+// app.get(/.*fly$/, (req, res) => {
+//     res.send("/.*fly$/");
+// }); 
 
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
