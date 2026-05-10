558dc13cd7d437b088e99352d77f3a13ce9bbd48
ajitesh2405 <ajitesh@priyaqubit.onmicrosoft.com>
2026-05-10 17:12:13 +0530

    second commit

--- src/app.js
+++ src/app.js
@@ -2,17 +2,24 @@ const express = require('express');
 
 const app = express(); //Create an instance of the express application
 
-app.get("/", (req, res) => {
-    res.send('Hello from the server!');
-}); //Middleware function to handle incoming requests and send a response
-
+//Middleware function to handle incoming requests and send a response
 app.get("/test", (req, res) => {
     res.send('Server is working!');
 });
 
-app.get("/hello", (req, res) => {
-    res.send('hello world');
+app.get("/user", (req, res) => {
+    res.send({firstName: "Ajitesh", lastName: "Sharma"});
 });
+
+app.post("/user", (req, res) => {
+    //Saving data to database
+    res.send("Data has been saved to the database");
+});
+
+app.delete("/user", (req, res) => {
+    //Deleting data from database
+    res.send("Data has been deleted from the database");
+})
 
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
